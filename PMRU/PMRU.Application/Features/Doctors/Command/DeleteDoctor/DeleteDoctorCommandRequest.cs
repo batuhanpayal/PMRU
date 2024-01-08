83b06dbf1e0a779271403b773ba145d2b98106a5
yagmurbrnk <yagmurbarankarakus@outlook.com>
2024-01-08 19:51:49 +0300

    Add Update-Create Doctor

---
+++ PMRU/PMRU.Application/Features/Doctors/Command/DeleteDoctor/DeleteDoctorCommandRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMRU.Application.Features.Doctors.Command.DeleteDoctor
+{
+    public class DeleteDoctorCommandRequest :IRequest
+    {
+        public int DoctorID { get; set; }
+
+
+    }
+}
